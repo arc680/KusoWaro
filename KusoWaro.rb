c400beed5d2d3bdfe2b853e4256c38f09ef17a6b
arc680 <maru.nslab@gmail.com>
2012-08-23 02:26:27 +0900

    設定画面から切り替えられるように

--- KusoWaro.rb
+++ KusoWaro.rb
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 # くそわろ
 
-Module.new do
-  Plugin.create(:KusoWaro).add_event_filter(:command){ |menu|
+Plugin.create(:KusoWaro) do
+  filter_command do |menu|
     menu[:KusoWaro] = {
       :slug => :KusoWaro,
       :name => 'くそわろ',
       :condition => lambda{ |m| m.message.repliable? },
       :exec => lambda{ |m|
-        Post.primary_service.update(:message=> "@#{m.message.user.idname} くそわろ",
+        if UserConfig[:reply_or_retweet] == "reply"
+          str = "@#{m.message.user.idname} くそわろ"
+        else
+          str = "そのツイット( '-^ )b クソワロタです RT @#{m.message.user.idname}: #{m.message} "
+        end
+        Post.primary_service.update(:message=> str,
                                     :replyto => m.message)
-         },
+      },
       :visible => true,
       :role => :message }
     [menu]
-  }
+  end
+
+  settings("くそわろ") do
+    select('種類', :reply_or_retweet) do
+      option "reply", "くそわろ"
+      option "retweet", "そのツイット"
+    end
+  end
 end
