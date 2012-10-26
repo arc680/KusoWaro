e96127735ac41584528c551b55d0fd2306e025ef
arc680 <maru.nslab@gmail.com>
2012-10-27 02:18:52 +0900

    mikutter0.2対応

--- KusoWaro.rb
+++ KusoWaro.rb
@@ -2,23 +2,16 @@
 # くそわろ
 
 Plugin.create(:KusoWaro) do
-  filter_command do |menu|
-    menu[:KusoWaro] = {
-      :slug => :KusoWaro,
-      :name => 'くそわろ',
-      :condition => lambda{ |m| m.message.repliable? },
-      :exec => lambda{ |m|
-        if UserConfig[:reply_or_retweet] == "reply"
-          str = "@#{m.message.user.idname} くそわろ"
-        else
-          str = "そのツイット( '-^ )b クソワロタです RT @#{m.message.user.idname}: #{m.message} "
-        end
-        Post.primary_service.update(:message=> str,
-                                    :replyto => m.message)
-      },
-      :visible => true,
-      :role => :message }
-    [menu]
+  command(:KusoWaro,
+          name: 'くそわろ',
+          condition: Plugin::Command[:CanReplyAll],
+          visible: true,
+          role: :timeline) do |m|
+    m.messages.map do |msg|
+      str = "@#{msg.message.user.idname} くそわろ"
+      Post.primary_service.update(:message => str,
+                                  :replyto => msg.message)
+    end
   end
 
   settings("くそわろ") do
